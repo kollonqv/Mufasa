e8d6961cb7bc6d540534ab249e09b6269bfb90fe
Kalle Ollonqvist <ollonqvistkalle@gmail.com>
2018-10-22 22:19:49 +0300

    Assignment Finished

--- tests/Person.java
+++ tests/Person.java
@@ -1,12 +1,68 @@
-import static org.junit.Assert.*;
-
-import org.junit.Test;
 
 public class Person {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	
+	private String firstName; 
+	private String lastName;
+	private String userName;
+	private String password;
+	private String email;
+	private String birthDate;
+	private String phoneNumber;
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	public String getUserName() {
+		return userName;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setBirthDate(String birthDate) {
+		this.birthDate = birthDate;
+	}
+	
+	public String getBirthDate() {
+		return birthDate;
+	}
+	
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	public String getPhoneNumber() {
+		return phoneNumber;
 	}
 
 }
